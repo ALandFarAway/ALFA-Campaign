c27bb7f639ad3df77afaf7b5324fc0f191801dad
FoamBats4All <mystrasstalker@gmail.com>
2013-01-29 22:18:36 -0800

    Prefer acr scripts over standard NWN2 scripts. Solves warnings native to Obsidian code.

--- project.mk
+++ project.mk
@@ -1,4 +1,4 @@
-NSS_INC_PATH=$(PROJECT_SOURCE_ROOT)\..\ALFA-Base-Resources\AdvancedScriptCompiler\scripts_X2;$(PROJECT_SOURCE_ROOT)\..\ALFA-Base-Resources\AdvancedScriptCompiler\Scripts_X1;$(PROJECT_SOURCE_ROOT)\..\ALFA-Base-Resources\AdvancedScriptCompiler\Scripts\Scripts;$(PROJECT_SOURCE_ROOT)\..\ALFA-Base-Resources\alfa2_acr.hak
+NSS_INC_PATH=$(PROJECT_SOURCE_ROOT)\..\ALFA-Base-Resources\alfa2_acr.hak;$(PROJECT_SOURCE_ROOT)\..\ALFA-Base-Resources\AdvancedScriptCompiler\scripts_X2;$(PROJECT_SOURCE_ROOT)\..\ALFA-Base-Resources\AdvancedScriptCompiler\Scripts_X1;$(PROJECT_SOURCE_ROOT)\..\ALFA-Base-Resources\AdvancedScriptCompiler\Scripts\Scripts
 NSS_COMPILER=$(PROJECT_SOURCE_ROOT)\..\ALFA-Base-Resources\AdvancedScriptCompiler\NWNScriptCompiler.exe
 
 MSBUILD_TOOLS_PATH=$(SYSTEMROOT)\Microsoft.NET\Framework\$(CLR_VERSION)
